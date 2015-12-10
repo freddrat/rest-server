1e4c77d852c33113a932f55eaf82a2487403c630
freddrat <alfred.leithold@gmail.com>
2015-12-10 14:33:45 +0100

    changes in body-parser declaration

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
-var jsonParser = bodyParser.json();
+// parse application/x-www-form-urlencoded 
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json 
+app.use(bodyParser.json());
 
-app.post('/EVENTS/:id', jsonParser, function(req,res,next) {
+app.post('/EVENTS/:id', function(req,res,next) {
 	console.log(req.body);
-	res.status(200).send(req.body);
+	res.status(200).send("Server responding");
 	next();
 });
 
-app.get('/configuration/:id', function(req, res, next) {
+app.post('/configuration/:id', function(req, res, next) {
 	//console.log('Id:'+req.params.id);
 	res.contentType('text/html');
-	res.send(200, 'id: '+req.params.id+'}');
+	res.send(200, '{id: '+req.params.id+'}');
 	next();
 });
 
